ad314c059e82dbe2cc99b1620e45b301c1e9f4da
KirillovAlexander1515 <kirillov_sasha98@mail.ru>
2019-12-05 14:05:02 +0300

    Made a project

---
+++ Vk-App/Vk-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vk-App
+//
+//  Created by Macbook on 05/12/2019.
+//  Copyright © 2019 AK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
